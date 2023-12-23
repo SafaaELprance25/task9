181785b7ec2c242f88ce724ae1f08f6982a08f15
safaa elprince <tryfree35@gmail.com>
2023-12-23 14:34:07 +0200

    task11

--- src/app/adduser/adduser.component.ts
+++ src/app/adduser/adduser.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Hero } from '../hero';
 
 @Component({
   selector: 'app-adduser',
@@ -6,5 +7,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./adduser.component.css']
 })
 export class AdduserComponent {
+  powers = ['Really Smart', 'Super Flexible',
+  'Super Hot', 'Weather Changer'];
+
+model = new Hero(18, 'Dr. IQ', this.powers[0], 'Chuck Overstreet');
+
+submitted = false;
+
+onSubmit() { this.submitted = true; }
 
 }
